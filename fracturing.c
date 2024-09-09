4618a37f5f082b0c03ba09adf8eed2fa1d25de5e
arielfevry <125998099+arielfevry@users.noreply.github.com>
2024-09-09 02:19:55 +0000

    final commit of assignment 1

--- fracturing.c
+++ fracturing.c
@@ -1,3 +1,19 @@
+/*********************************
+fracturing.c 
+Author: Ariel Fevry
+Date: 9/8/2024
+Class: COP3223, Juan Parra
+Purpose: The program accepts 4 user inputs as (x,y) points on a circle and does
+         five computations on the inputs: circumference, area, distance, height, width.
+         The purpose of the program is to practice creating functions as a way to 
+         reduce redudancy and break a problem down into its parts. 
+Inputs: 2 (x,y) coordinates
+Ouput: The distance between the coordiantes, area of the circle, perimeter of the circle, 
+       width of the circle, and height of the circle. The user inputed coordinates will also
+       be displayed back in setence form to the terminal. 
+Limitations: No input validation to prevent program from crashing. 
+*********************************/
+
 // import header files standard i/o and math library functions
 #include <stdio.h>
 #include <math.h>
@@ -8,8 +24,7 @@ double askUserForInput(){
     // variable to hold the user input
     double userInputedDimension = 0;
 
-    // prompt user and save the inputed value to variable
-    printf("\tEnter in dimension: ");
+    // save the inputed value to variable
     scanf("%lf", &userInputedDimension);
 
     // return dimension
@@ -73,24 +88,25 @@ double calculateArea(){
 }
 
 double calculateWidth(){
-    // declare and initilize the points 
-    double x1 = askUserForInput();
-    double x2 = askUserForInput();
+    // distance formula to calculate width of circle (diameter)
+    double width = distanceFormula();
 
-    return x2 - x1; // subtract to to get width
+    printf("\nThe width of the city encompassed by your request is %lf\n", width);
+
+    return 1;
 }
 
 double calculateHeight(){
-    // delcare and initilize points 
-    double y1 = askUserForInput();
-    double y2 = askUserForInput();
+    // distance formula to calculate height of circle (diameter)
+    double height = distanceFormula();
 
-    return y2 - y1; // subtract to get height
+    printf("\nThe height of the city encompassed by your request is %lf\n", height);
+
+    return 1;
 }
 
 int main(int argc, char **argv){
     //prompt user and call functions
-
     printf("Calculate the distance between two points on a circle by entering the x and y of the points respectively\n");
     calculateDistance();
 
@@ -103,8 +119,8 @@ int main(int argc, char **argv){
     printf("\nCaculate the width of a city by entering the x and y of two points respectively\n");
     calculateWidth();
 
-    printf("\nCalculate the height of a city by entering the x and y of two points respectively\n")
+    printf("\nCalculate the height of a city by entering the x and y of two points respectively\n");
     calculateHeight();
-
+    
     return 0;
 }
